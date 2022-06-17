a5cba1f586ed858fd0e4b007a297174d201ba13f
Gabriel Petry <gabriel.petry@aziontech.com.br>
2022-06-17 14:25:56 -0300

    add linux brew

--- zshrc_lazy/build_functions/0.brew.sh
+++ zshrc_lazy/build_functions/0.brew.sh
@@ -1,3 +1,10 @@
 
 echo
-/usr/local/bin/brew shellenv 2>/dev/null || /opt/homebrew/bin/brew shellenv
+/usr/local/bin/brew shellenv 2>/dev/null || /opt/homebrew/bin/brew shellenv || /home/linuxbrew/.linuxbrew/bin/brew shellenv
+
+test -d $HOME/perl5 || PERL_MM_OPT="INSTALL_BASE=$HOME/perl5" cpan local::lib
+perl -I$HOME/perl5/lib/perl5 -Mlocal::lib=$HOME/perl5
+
+export GOPATH=$HOME/go
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
